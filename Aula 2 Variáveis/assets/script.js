20b399fcc6eeb5d67cca28c5eef28d00e6092f35
Eduardo Eugenio B Moises <eduebm@gmail.com>
2022-06-23 17:57:28 -0300

    aula 2 completa

--- Aula 2 Variáveis/assets/script.js
+++ Aula 2 Variáveis/assets/script.js
@@ -1,46 +1,50 @@
 //Tipos primitivos
 
 //identificando uma condição boolean
-/*var V= true;
+/*
+
+var V= true;
 var F= false;
 console.log(typeof(V));
-console.log(typeof (F));
- //Identificando um número
+console.log(typeof (F));*/
 
+ //Identificando um número
+/*
  var Número= 1;
  console.log(typeof(1));
-
+*/
  //identificando uma string
-
+/*
  var nome= "Eduardo";
  console.log(typeof(nome));
- 
+ */
  //função
+ /*
  var função= function(){}
  console.log(typeof (função));
-
+*/
 //como declarar
-
+/*
 var variavel2 = 'Chuck norris'
 variavel2 = 'chublal'
 console.log('variavel2')
 
-let variavel3 = 'rambo'
-variavel3 = 'pepapig'
-console.log('variavel3')
+let variavel3 = 'rambo';
+variavel3 = 'pepapig';
+console.log('variavel3');
 
-const constante = Edu
-console.log(constante)
+const constante = Edu;
+console.log(constante);
 
 é uma variável constante não pode 
 ser alterada ex:
-const constante= 'edu';
+const constante = 'edu';
 constante = 'eduardo';
 não será mudado o valor
 da constante para eduardo
-
+*/
 //escopo global
-
+/*
 var escopoglobal = 'global';
 console.log (escopoglobal);
 
@@ -48,54 +52,107 @@ function escopoLocal(){
     let escopolocalinterno= 'Local';
 console.logo(escopolocalinterno);
 }
-
+*/
 // atribuição
+/*
 var atribuicao = nome;
-
+*/
 //comparacao
+/*
 var comparacao= '0' == 0;
 console.log(comparacao);
-
+*/
 //comparacao identica
+/*
 var comparacaoidentica = '0' === 0;
 console.log(comparacaoidentica);
-
+*/
 //operadores aritmeticos
 
 //adição
+/*
 var adiçao = 1+1 ;
 console.log(adicao)
 irá imprimir o valor 2
-
+*/
 //subtracao
-
+/*
 var subtracao = 2 - 1;
 console.log(subtracao);
 irá imprimir o valor 1
-
+*/
 //divisão
-
+/*
 var divisao = 6/2;
 console.log(divisao);
 irá retornar o valor 3
-
-divisao inteira
-
+*/
+//divisao inteira
+/*
 var divisaointeira= 5%2;
 console.log(divisaointeira);
 retornará o resto da divisao que é
 1
 
-
+*/
 //multiplicacao
+/*
 var multiplicacao = 2*3;
 console.logo (multiplicacao);
 ira retornar o valor 6
-
+*/
 //potenciacao
-
+/*
 var potenciacao = 5**2;
 console.log(potenciacao);
 retornará o valor 25
+*/
+//operadores de comparacao
 
-//operadores de compraracao
+//maior que
+/*
+var maiorque = 5 > 1;
+consolo.log(maiorque);
+*/
+//menor que
+/*
+var menorque = 5 < 2;
+console.log(menorque);
+nesse caso o console retorna que
+a afirmação é falsa
+*/
+//maior ou igual que
+/*
+var maiorouigualque = 5 >= 2;
+console.log(maiorouigualque);
+true
+*/
+
+// menor ou igual
+/*
+var menorouigualque = 5 <= 2;
+console.log(menorouigualque);
+false
+*/
+
+//operadores lógicos
+
+// operador e
+/*
+var e = true && true;
+console.log(e);
+*/
+
+// operador ou
+/*
+var ou = false || false;
+console.log(ou);
+*/
+
+//negação
+/*
+var nao = !true ;
+console.log(nao);
+*/
+
+//fim da aula 2
