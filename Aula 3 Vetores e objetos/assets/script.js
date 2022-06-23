20fbaa3393524274c9b91935c2f20536c976d128
Eduardo Eugenio B Moises <eduebm@gmail.com>
2022-06-23 19:40:52 -0300

    metade da Aula 3

---
+++ Aula 3 Vetores e objetos/assets/script.js
@@ -0,0 +1,70 @@
+//oque são vetores ou arrays
+
+//como declarar arrays
+
+/*
+let array = [1,'string',false, true];
+console.log(array);
+*/
+
+//Pode guardar varios tipos de dados
+
+let array = ['string', 2, 3, false, ['array1'],['array2'],['array3'], true]
+/*console.log(array)*/
+
+//forEach
+
+/*array.forEach(function(item, index){console.log(item, index)});
+*/
+/*
+
+//.push (adiciona um novo item ao final do array existente)
+
+array.push(['arraynovopush']);
+console.log(array); 
+
+
+
+*/
+
+//.pop (remove um item final do array existente)
+/*
+array.pop();
+console.log(array);
+*/
+
+//.shift (remove o item do início do array existente)
+/*
+array.shift();
+console.log(array);
+*/
+//.unshift (adiciona um item no início do array)
+
+/*
+array.unshift(true);
+console.log(array);
+*/
+
+//.indexof (atraves do console.log(array.indexOf(item)); mostra a ordem no qual o ítem esta)
+
+/*
+console.log(array.indexOf(true));
+*/
+
+//splice (apaga um intervalo de itens do array)
+
+/*
+array.splice(0, 4);
+console.log(array);
+*/
+
+//.slice (adiciona parte do intervalo informado de um array existente)
+
+let novoarray = array.slice(3, 7);
+console.log(novoarray);
+
+
+
+
+
+
